388c74f5c5ed31d4c3a32c3e72ac208611caab25
Todor Ilivanov <t.ilivanov@gmail.com>
2020-08-15 13:10:12 +0300

    Added add track by name feature

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def index():
         # Step 1. Visitor is unknown, give random ID
         session['uuid'] = str(uuid.uuid4())
 
-    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private playlist-modify-public',
                                                 cache_path=session_cache_path(), 
                                                 show_dialog=True)
 
@@ -50,57 +50,6 @@ def index():
            f'<a href="/playlists">my playlists</a> | ' \
            f'<a href="/currently_playing">currently playing</a> | ' \
 		   f'<a href="/current_user">me</a>' \
-
-
-@app.route('/sign_out')
-def sign_out():
-    os.remove(session_cache_path())
-    session.clear()
-    try:
-        # Remove the CACHE file (.cache-test) so that a new user can authorize.
-        os.remove(session_cache_path())
-    except OSError as e:
-        print ("Error: %s - %s." % (e.filename, e.strerror))
-    return redirect('/')
-
-#TODO: does not need to be a route
-@app.route('/search_track_id')
-def search_track_id():
-    if request.args.get('name'):
-        track_name = request.args.get('name')
-
-        sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-        first_result = sp.search(track_name)['tracks']['items'][0]
-        pprint.pprint(first_result['id'])
-
-        return first_result['id']
-    else:
-        # TODO: Handle error properly
-        return "No track name specified."
-
-@app.route('/create_playlist')
-def create_playlist():
-    playlist_names = get_playlist_names()
-    #pprint.pprint(playlist_names)
-    if PLAYLIST_NAME not in playlist_names:
-        auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=session_cache_path())
-        sp = spotipy.Spotify(auth_manager)
-        user_id = sp.me()['id']
-        sp.user_playlist_create(user=user_id, 
-            name=PLAYLIST_NAME, 
-            public=True, 
-            description="Songs recognized from screenshots are added to this playlist."
-        )
-
-        return f'Playlist {PLAYLIST_NAME} created.'
-    else:
-        # TODO: Handle properly
-        return "Playlist already exists."
-
-def get_playlist_names():
-    playlists = get_playlists()
-    playlist_names = [pl['name'] for pl in playlists['items']]
-    return playlist_names
 
 @app.route('/playlists')
 def get_playlists():
@@ -131,6 +80,76 @@ def current_user():
     spotify = spotipy.Spotify(auth_manager=auth_manager)
     return spotify.current_user()
 
+
+@app.route('/sign_out')
+def sign_out():
+    os.remove(session_cache_path())
+    session.clear()
+    try:
+        # Remove the CACHE file (.cache-test) so that a new user can authorize.
+        os.remove(session_cache_path())
+    except OSError as e:
+        print ("Error: %s - %s." % (e.filename, e.strerror))
+    return redirect('/')
+
+@app.route('/create_playlist')
+def create_playlist():
+    playlist_names = get_playlist_names()
+    #pprint.pprint(playlist_names)
+    if PLAYLIST_NAME not in playlist_names:
+        auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=session_cache_path())
+        if not auth_manager.get_cached_token():
+            return redirect('/')
+        spotify = spotipy.Spotify(auth_manager=auth_manager)
+        user_id = spotify.me()['id']
+        response = spotify.user_playlist_create(user=user_id, 
+            name=PLAYLIST_NAME, 
+            public=True, 
+            description="Songs recognized from screenshots are added to this playlist."
+        )
+        print(f'Playlist {PLAYLIST_NAME} created.')
+        # TODO: handle errors
+        return response['id']
+    else:
+        # TODO: Handle properly
+        print("Playlist already exists.")
+        return get_playlist_id(PLAYLIST_NAME)
+
+def get_playlist_id(name):
+    playlists = get_playlists()
+    playlist = [pl for pl in playlists['items'] if pl['name'] == name][0]
+    pprint.pprint(playlist)
+    return playlist['id']
+
+def get_playlist_names():
+    playlists = get_playlists()
+    playlist_names = [pl['name'] for pl in playlists['items']]
+    return playlist_names
+
+def search_track_id(track_name):
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    first_result = sp.search(track_name)['tracks']['items'][0]
+    pprint.pprint(first_result['id'])
+    return first_result['id']
+
+@app.route('/add_track')
+def add_track():
+    if request.args.get('name'):
+        track_name = request.args.get('name')
+        track_id = search_track_id(track_name)
+        playlist_id = create_playlist()
+
+        auth_manager = spotipy.oauth2.SpotifyOAuth(cache_path=session_cache_path())
+        if not auth_manager.get_cached_token():
+            return redirect('/')
+            
+        sp = spotipy.Spotify(auth_manager=auth_manager)
+        sp.playlist_add_items(playlist_id, [track_id])
+
+        return "Song added."
+    else:
+        # TODO: Handle error properly
+        return "No track name specified."
 
 '''
 Following lines allow application to be run more conveniently with
